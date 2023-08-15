7e682430d9501780ad9d3cde2897545243827475
Kalen Wallin <31481852+kalenwallin@users.noreply.github.com>
2023-08-14 19:16:39 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-    // your configuration options here...
-    // https://docs.astro.build/en/reference/configuration-reference/
+    experimental: {
+        viewTransitions: true,
+    },
 });
