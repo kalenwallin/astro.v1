9418183313fce4a7a69106fa0379464979b31939
Kalen Wallin <31481852+kalenwallin@users.noreply.github.com>
2023-08-24 18:45:32 -0500

    Reset tsconfig

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,1 @@
 /// <reference types="astro/client" />
-declare global {
-    var myString: string;
-    function myFunction(): boolean;
-}
-
-export {};
-interface Window {
-    myFunction(): boolean;
-}
