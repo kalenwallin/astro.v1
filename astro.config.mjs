d83a1b6b1c287ec69d19ea45f171821bbb24e512
Kalen Wallin <31481852+kalenwallin@users.noreply.github.com>
2023-09-25 17:54:37 -0500

    Set Career Experience as the landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
     redirects: {
-        "/": "/websites/",
+        "/": "/career experience/",
     },
     output: "static",
     adapter: vercel({
